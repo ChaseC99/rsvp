9a1c817448956a03f97a5daf3d8f66f642643bd9
Chase Carnaroli <chasecarnaroli@gmail.com>
2023-08-13 05:23:29 -0700

    Fetch event data based on url path

--- app/[event]/page.tsx
+++ app/[event]/page.tsx
@@ -1,4 +1,3 @@
-import { sql } from "@vercel/postgres";
 import EventPage from "./event";
 import { EVENT_QUERY } from "../queries";
 
@@ -6,20 +5,21 @@ import type {
     Event,
 } from "../types";
 
-async function loadEvent(): Promise<Event> {
-    const result = await EVENT_QUERY('event1');
-
-    const event = result.rows[0].event;
-
-    return event;
+async function loadEvent(id: string): Promise<Event> {
+    const result = await EVENT_QUERY(id);
+    return result.rows[0].event;
 }
 
 // This is a server-side rendered component,
 // so that it can easily load data from the database
 // The event data is then passed into EventPage,
 // which is a client-side rendered component
-export default async function EventPageLoader() {
-    const event = await loadEvent();
+export default async function EventPageLoader(
+    props: { params: { event: string } },
+) {
+    // Get the event id from the URL
+    const { params: { event: eventId } } = props;
+    const event = await loadEvent(eventId);
 
     return <EventPage event={event} />
 }
