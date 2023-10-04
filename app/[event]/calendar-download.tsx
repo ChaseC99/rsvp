49828766452d31345e72eeffceed0d5d74643535
Chase Carnaroli <chasecarnaroli@gmail.com>
2023-10-04 01:22:30 -0700

    New button to add event to Google Calendar (#6)
    This adds a new button to add an event to google calendar.
    After RSVP-ing to an event, the tooltip for the button will briefly show.

---
+++ app/[event]/calendar-download.tsx
@@ -0,0 +1,45 @@
+import { Button, Tooltip } from "@mui/material";
+import InsertInvitationIcon from '@mui/icons-material/InsertInvitation';
+import { Event } from "../types"
+
+type CalendarDownloadProps = {
+    event: Event,
+    tooltipOpen?: boolean,
+    onOpen?: () => void,
+    onClose?: () => void,
+}
+
+export default function CalendarDownload({ event, tooltipOpen, onOpen, onClose }: CalendarDownloadProps) {
+    const { title, location } = event;
+    // Add the event url to the description
+    const description = event.description ? event.description + `\n\nhttps://rsvp.chase.link/${event.id}` : `https://rsvp.chase.link/${event.id}`;
+    // Google Calendar requires the date to be in a specific format
+    // YYYYMMDDTHHmmSSZ/YYYYMMDDTHHmmSSZ
+    const date = event.date.toISOString().replace(/-|:|\.\d+/g, '');
+
+    // Google Calendar add event url reference: https://github.com/InteractionDesignFoundation/add-event-to-calendar-docs/blob/main/services/google.md#parameters
+    const gcalAddEventUrl = new URL('https://www.google.com/calendar/render');
+    gcalAddEventUrl.searchParams.append('action', 'TEMPLATE');
+    gcalAddEventUrl.searchParams.append('text', title);
+    gcalAddEventUrl.searchParams.append('dates', `${date}/${date}`);
+    gcalAddEventUrl.searchParams.append('details', description);
+    gcalAddEventUrl.searchParams.append('location', location);
+
+    return (
+        <Tooltip 
+            title="Add to Google Calendar" 
+            arrow 
+            open={tooltipOpen} 
+            onOpen={onOpen} 
+            onClose={onClose}
+        >
+            <Button
+                href={gcalAddEventUrl.toString()}
+                target="_blank"
+                rel="noreferrer"
+            >
+                <InsertInvitationIcon />
+            </Button>
+        </Tooltip>
+    )
+}
